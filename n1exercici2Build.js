14a6d4dadf5f432871d0cc9b1baf415ea66daa5d
Joren <97386268+Aebal34@users.noreply.github.com>
2023-06-09 11:38:24 +0200

    n1exercici2 done

---
+++ n1exercici2Build.js
@@ -0,0 +1,7 @@
+use pizzeriailcapo
+
+db.createCollection("Stores")
+db.createCollection("Customer")
+db.createCollection("Orders")
+db.createCollection("OrderProducts")
+db.createCollection("Products")
